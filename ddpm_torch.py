4996b9e594b2aa3da4a3cca8782b3cd1d5ede269
heepengpeng <heepengpeng@foxmail.com>
2023-07-05 15:56:36 +0800

    [feat] add ms , torch demo

--- ddpm_torch.py
+++ ddpm_torch.py
@@ -1,18 +1,12 @@
 import time
 
-import matplotlib.pyplot as plt
-import numpy as np
 import torch
 from sklearn.datasets import make_s_curve
 
 s_curve, _ = make_s_curve(10 ** 4, noise=0.1)
 s_curve = s_curve[:, [0, 2]] / 10.0
-print("shape of moons: ", np.shape(s_curve))
 data = s_curve.T
 
-fig, ax = plt.subplots()
-ax.scatter(*data, color='red', edgecolor='white')
-ax.axis('off')
 dataset = torch.Tensor(s_curve).float()
 
 num_steps = 100
